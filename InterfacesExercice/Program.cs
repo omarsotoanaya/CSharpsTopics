f2130ba8c32f26d4998ce3a4dd3455c434a000d6
Omar Alejandro Soto <omar.soto@softtek.com>
2017-08-24 16:26:48 -0700

    FirstTest
    adding interfaces exercices

---
+++ InterfacesExercice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkFlow wf = new WorkFlow();
+            wf.Add(new SendNotification());
+            wf.Add(new SMSNotification());
+            wf.Add(new CloudVideo());
+            wf.Run();
+
+            Console.ReadLine();
+
+        }
+    }
+}
